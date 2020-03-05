252191714d645166090fdfd6771efd9c3d877259
Riya Prasad <ripras@microsoft.com>
2020-03-05 14:42:02 +0530

    Edited Routing for app1 and app2

--- app1/src/app/app-routing.module.ts
+++ app1/src/app/app-routing.module.ts
@@ -4,16 +4,27 @@ import { EmptyRouteComponent } from './empty-route.component';
 import { APP_BASE_HREF } from '@angular/common';
 import { AboutComponent } from './about/about.component';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   // { path: '', pathMatch: 'full', component: EmptyRouteComponent },
   // { path: '**', redirectTo: '' },
   // { path: '**', component: EmptyRouteComponent },
-  {
-    path: 'app1', component: AppComponent,
-    // children: [{ path: 'about', component: AboutComponent}]
+  // {
+  //   path: 'app1', redirectTo: 'app1/about',
+  //   // children: [{ path: 'about', component: AboutComponent}]
+  // },
+  {path: 'app1',
+    children: [
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent }
+    ]
   },
-  {path: 'app1/about', component: AboutComponent}
+  { path: '', redirectTo: 'app1/about', pathMatch: 'full' },
+  // {path: 'app1/about', component: AboutComponent},
+  // { path: 'app1/contact', component: ContactComponent },
+  // { path: '', redirectTo: 'app1/about', pathMatch: 'full'},
+  { path: '**', component: EmptyRouteComponent }
   // { path: '**', redirectTo: '' },
   // { path: '', pathMatch: 'full', component: AppComponent }
 ];
--- app2/src/app/app-routing.module.ts
+++ app2/src/app/app-routing.module.ts
@@ -2,8 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  //  { path: '**', component: EmptyRouteComponent },
+  // {
+  //   path: 'app2', component: AppComponent
+  // },
+  {
+    path: 'app2', redirectTo: 'app2/about',
+  },
+  { path: 'app2/about', component: AboutComponent },
+  { path: 'app2/contact', component: ContactComponent },
+  { path: '', redirectTo: 'app2/about', pathMatch: 'full' },
   { path: '**', component: EmptyRouteComponent },
 ];
 
