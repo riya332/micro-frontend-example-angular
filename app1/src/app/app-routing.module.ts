bac17171392a70cceac65a43e4238ad1a7f6381c
Riya Prasad <ripras@microsoft.com>
2020-03-05 11:58:16 +0530

    Updated Routes added subroute

--- app1/src/app/app-routing.module.ts
+++ app1/src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { EmptyRouteComponent } from './empty-route.component';
 import { APP_BASE_HREF } from '@angular/common';
 import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   // { path: '', pathMatch: 'full', component: EmptyRouteComponent },
   // { path: '**', redirectTo: '' },
-  { path: '**', component: EmptyRouteComponent },
-  {path: 'app1/about/', component: AboutComponent}
+  // { path: '**', component: EmptyRouteComponent },
+  {
+    path: 'app1', component: AppComponent,
+    // children: [{ path: 'about', component: AboutComponent}]
+  },
+  {path: 'app1/about', component: AboutComponent}
+  // { path: '**', redirectTo: '' },
+  // { path: '', pathMatch: 'full', component: AppComponent }
 ];
 
 @NgModule({
