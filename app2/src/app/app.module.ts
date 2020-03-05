155575c488e7ecb6248596a3469b02458c12bd3a
Riya Prasad <ripras@microsoft.com>
2020-03-05 13:14:25 +0530

    Added 2 components to App2

--- app2/src/app/app.module.ts
+++ app2/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmptyRouteComponent } from './empty-route.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmptyRouteComponent
+    EmptyRouteComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
